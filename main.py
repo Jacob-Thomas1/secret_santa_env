8da934f97a66a9199124824e86ddf804ca6e97af
Jacob-Thomas1 <35864668+Jacob-Thomas1@users.noreply.github.com>
2025-06-20 11:07:46 +0530

    removes debug prints

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import random
 
+from models.Employee import Employee
 from models.SecretSanta import SecretSanta
 from utils.excelUtils import return_employee_list
 
@@ -13,52 +14,32 @@ from utils.excelUtils import return_employee_list
 
 
 def main():
-    file_path = "input/Employee-List.xlsx"
-    previous_santa_file_path = "input/Secret-Santa-Game-Result-2023.xlsx"
+    file_path = 'input/Employee-List.xlsx'
+    previous_santa_file_path = 'input/Secret-Santa-Game-Result-2023.xlsx'
     employees = return_employee_list(file_path, previous_santa_file_path)
-
-    print("EMPLOYEES:", employees)
-
+    
     secret_santa_manager = SecretSanta(employees)
 
     secret_santa_manager.assign_secret_santa()
 
-    print(
-        "           ******       Current (2024) Secret Santa assignments:        ****** \n"
-    )
+    print("           ******       Current (2024) Secret Santa assignments:        ****** \n")
     for employee in secret_santa_manager.employees:
-        print(
-            f"{employee.employee_name} ({employee.employee_email}) -> {employee.secret_santa.employee_name} ({employee.secret_santa.employee_email})"
-        )
+        print(f"{employee.employee_name} ({employee.employee_email}) -> {employee.secret_santa.employee_name} ({employee.secret_santa.employee_email})")
     print("\n\n")
-
-    df = pd.DataFrame(
-        {
-            "Employee_Name": [
-                emp.employee_name for emp in secret_santa_manager.employees
-            ],
-            "Employee_EmailID": [
-                emp.employee_email for emp in secret_santa_manager.employees
-            ],
-            "Secret_Child_Name": [
-                emp.secret_santa.employee_name for emp in secret_santa_manager.employees
-            ],
-            "Secret_Child_EmailID": [
-                emp.secret_santa.employee_email
-                for emp in secret_santa_manager.employees
-            ],
-        }
-    )
-
+    
+    df = pd.DataFrame({
+        'Employee_Name': [emp.employee_name for emp in secret_santa_manager.employees],
+        'Employee_EmailID': [emp.employee_email for emp in secret_santa_manager.employees],
+        'Secret_Child_Name': [emp.secret_santa.employee_name for emp in secret_santa_manager.employees],
+        'Secret_Child_EmailID': [emp.secret_santa.employee_email for emp in secret_santa_manager.employees]
+    })
+    
     import os
-
-    if not os.path.exists("output"):
-        os.makedirs("output")
-
-    output_file_path = "output/Secret-Santa-Game-Result-2024.xlsx"
+    if not os.path.exists('output'):
+        os.makedirs('output')
+    
+    output_file_path = 'output/Secret-Santa-Game-Result-2024.xlsx'
     df.to_excel(output_file_path, index=False)
     print(f"Secret Santa assignments saved to {output_file_path}")
-
-
 if __name__ == "__main__":
     main()
